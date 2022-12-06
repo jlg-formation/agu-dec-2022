dc45ec66364412025c188e88e71d7da41bdd7856
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-12-06 14:58:56 +0100

    dev: routing lazy loading

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
 
@@ -12,10 +12,19 @@ const routes: Routes = [
     component: LegalComponent,
     path: 'legal',
   },
+  {
+    path: 'stock',
+    loadChildren: () =>
+      import('./stock/stock.module').then((m) => m.StockModule),
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
