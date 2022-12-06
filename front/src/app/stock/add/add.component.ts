245ac33ee5e01201e8920739c49a8a9b2cd8b719
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-12-06 17:39:53 +0100

    dev: started to code form

--- front/src/app/stock/add/add.component.ts
+++ front/src/app/stock/add/add.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
 export class AddComponent {
-
+  f = new FormGroup({
+    name: new FormControl('Truc', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    price: new FormControl(0, Validators.required),
+    qty: new FormControl(1, Validators.required),
+  });
 }
--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Article } from '../interfaces/article';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent {
-
+  articles: Article[] = [
+    { name: 'Tournevis', price: 2.99, qty: 123 },
+    { name: 'Pelle', price: 3, qty: 45 },
+  ];
 }
