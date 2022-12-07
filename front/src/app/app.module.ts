463f2fd0986069af33d544533417557a90b66f4f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-12-07 14:42:27 +0100

    dev: added http client

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { LegalComponent } from './routes/legal/legal.component';
 import { HomeComponent } from './routes/home/home.component';
+import { HttpArticleService } from './services/http-article.service';
+import { ArticleService } from './services/article.service';
 
 @NgModule({
   declarations: [AppComponent, LegalComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule, HttpClientModule],
+  providers: [{ useClass: HttpArticleService, provide: ArticleService }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ front/src/app/services/http-article.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ArticleService } from './article.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpArticleService extends ArticleService {
+  constructor(private http: HttpClient) {
+    super();
+    console.log('instantiate http article service');
+    this.refresh();
+  }
+
+  override async refresh(): Promise<void> {
+    this.http.get('http://localhost:3000/api/articles');
+    console.log('refreshed');
+  }
+}
