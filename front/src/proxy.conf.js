65cde539c21926a0e34ed3f2860e89817633b0da
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-12-08 09:50:51 +0100

    dev: added log in proxy

---
+++ front/src/proxy.conf.js
@@ -0,0 +1,23 @@
+const winston = require("winston");
+
+function logProvider() {
+  return winston.createLogger({
+    level: "debug",
+    format: winston.format.combine(
+      winston.format.splat(),
+      winston.format.simple()
+    ),
+    transports: [new winston.transports.Console()],
+  });
+}
+
+const proxyConfig = {
+  "/api": {
+    target: "http://localhost:3000",
+    secure: false,
+    logLevel: "debug",
+    logProvider: logProvider,
+  },
+};
+
+module.exports = proxyConfig;
