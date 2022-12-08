587aad4596dfc3975a34dcf03204bb34c50c0535
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-12-08 17:02:21 +0100

    dev: console.log

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,6 +1,11 @@
+import { isDevMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+
+if (!isDevMode()) {
+  console.log = () => {};
+}
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
