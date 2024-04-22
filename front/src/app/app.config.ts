72f0d55ff7222a9eaaa3505c4fb25c534cdd6477
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-04-22 14:50:27 +0200

    dev: added http

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HttpArticleService } from './services/http-article.service';
+import { ArticleService } from './services/article.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    { useClass: HttpArticleService, provide: ArticleService },
+  ],
 };
