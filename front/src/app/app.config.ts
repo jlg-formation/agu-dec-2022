a5939e5a891b412304df3ab2109e9c6658f0d907
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-04-22 15:02:16 +0200

    dev: app config with DI

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,15 +1,26 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  ApplicationConfig,
+  DEFAULT_CURRENCY_CODE,
+  LOCALE_ID,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { HttpArticleService } from './services/http-article.service';
 import { ArticleService } from './services/article.service';
+import { registerLocaleData } from '@angular/common';
+import localeFrBE from '@angular/common/locales/fr-BE';
+
+// the second parameter 'fr' is optional
+registerLocaleData(localeFrBE, 'fr-BE');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withFetch()),
     { useClass: HttpArticleService, provide: ArticleService },
+    { provide: LOCALE_ID, useValue: 'fr-BE' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
   ],
 };
