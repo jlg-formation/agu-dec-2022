df1697744cf740992aac50b6aa9cb52e718e44a8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-04-23 14:48:29 +0200

    dev: template async btn done

--- front/src/app/widget/async-btn/async-btn.component.ts
+++ front/src/app/widget/async-btn/async-btn.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  IconDefinition,
+  faCircleNotch,
+} from '@fortawesome/free-solid-svg-icons';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-async-btn',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './async-btn.component.html',
-  styleUrl: './async-btn.component.scss'
+  styleUrl: './async-btn.component.scss',
 })
 export class AsyncBtnComponent {
+  @Input()
+  label = '';
 
+  @Input()
+  icon = faCircleNotch;
+
+  isInProgress = false;
+
+  faCircleNotch = faCircleNotch;
+
+  doSomethingLong(): void {
+    of(undefined).pipe().subscribe();
+  }
 }
