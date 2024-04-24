2c4e12b9915cb001f1dbeff9d697f99994d89b20
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-04-24 15:10:03 +0200

    dev: added dynamic import for lazy loading

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
-import { LegalComponent } from './routes/legal/legal.component';
 
 export const routes: Routes = [
   {
@@ -8,7 +7,8 @@ export const routes: Routes = [
     path: '',
   },
   {
-    component: LegalComponent,
+    loadComponent: () =>
+      import('./routes/legal/legal.component').then((m) => m.LegalComponent),
     path: 'legal',
   },
   {
